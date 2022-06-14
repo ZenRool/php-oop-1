baaaee4c4e4d948eeb9f0219089355ab65450ff1
Zenrool <lorenzopapperini@hotmail.it>
2022-06-14 07:21:27 +0200

    Stampa i vari film in pagina con il foreach

--- index.php
+++ index.php
@@ -12,9 +12,19 @@ class Movie {
         $this->minutes = $_minutes;
     }
     public function getHours() {
-        return floor($year/60) . ":" . $year % 60;
+        return floor($this->minutes/60) . ":". ($this->minutes % 60 <10 ? "0" : "") . $this->minutes % 60;
     }
 }
+//Objs
+$movies = [];
+$fc = new Movie("Fight Club", ["drammatico", "commedia", "thriller", "noir", "azione"], 1999, 139);
+$movies[] = $fc;
+$vertigo = new Movie("Vertigo", ["giallo", "sentimentale", "thriller"], 1958, 128);
+$movies[] = $vertigo;
+$orange = new Movie("Arancia Meccanica", ["drammatico", "grottesco", "fantascienza"], 1971, 136);
+$movies[] = $orange;
+$pf = new Movie("Pulp Fiction", ["drammatico", "commedia", "thriller", "noir", "gangster"], 1994, 154);
+$movies[] = $pf;
 ?>
 <!DOCTYPE html>
 <html lang="it">
@@ -25,7 +35,35 @@ class Movie {
     <!-- Title -->
     <title>Movies</title>
 </head>
-<body>
-    
+<body style = "font-family: sans-serif">
+    <h1>Lista dei film in catalogo</h1>
+    <hr>
+    <ul>
+        <?php foreach($movies as $movie) {?>
+        <li>
+            <h2>
+                <?php echo $movie->movieName; ?>
+            </h2>
+            <p>
+                Anno: 
+                <?php echo $movie->year;?>
+            </p>
+            <p>
+                Generi: 
+                <?php
+                    $last = count($movie->genre) - 1;
+                    foreach($movie->genre as $index=>$elem) {
+                        echo $elem; 
+                        echo ($index === $last) ? "" : ", ";
+                    }              
+                ?>
+            </p>
+            <p>
+                Durata: 
+                <?php echo $movie->getHours();?>
+            </p>
+        </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
